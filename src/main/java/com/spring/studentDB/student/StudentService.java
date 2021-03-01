c9a475e3ed2618568e8880f30d13809b08e5cc9b
kevinlee <kevinlee3618@gmail.com>
2021-02-28 19:04:30 -0800

    Added additional corner cases for validating input to student fields

--- src/main/java/com/spring/studentDB/student/StudentService.java
+++ src/main/java/com/spring/studentDB/student/StudentService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -22,11 +23,31 @@ public class StudentService {
     }
 
     public void addNewStudent(Student student) {
-        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+
+        String firstname = student.getFirstname();
+        String lastname = student.getLastname();
+        String email = student.getEmail();
+        LocalDate dob = student.getDob();
+
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+        if(!(firstname != null && firstname.length() > 0) || !(lastname != null && lastname.length() > 0)) {
+            throw new IllegalStateException("Invalid Name");
+        }
+
+        if(!(email != null && email.length() > 0) || !email.matches(regex)) {
+            throw new IllegalStateException("Invalid Email");
+        }
 
         if(studentOptional.isPresent()) {
             throw new IllegalStateException("Email Taken");
         }
+
+        if(LocalDate.now().isBefore(dob)) {
+            throw new IllegalStateException("Invalid Date of Birth");
+        }
+
         studentRepository.save(student);
     }
 
@@ -47,7 +68,7 @@ public class StudentService {
                 )
         );
 
-        if(firstname != null && firstname.length() > 0 && lastname != null && lastname.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+        if((firstname != null && firstname.length() > 0) && (lastname != null && lastname.length() > 0) && !Objects.equals(student.getEmail(), email)) {
             student.setFirstname(firstname);
             student.setLastname(lastname);
         }
